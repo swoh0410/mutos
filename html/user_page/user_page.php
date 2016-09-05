9a293898e75bae55d6c3434e34f46baf576e6ecd
SWOH (Sol) <swoh90@gmail.com>
2016-09-05 14:53:18 +0900

    <구글맵 API받아서 해보는중.> load하면 맵이 안 보임//

--- html/user_page/user_page.php
+++ html/user_page/user_page.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="initial-scale=1.0">
 	<meta charset = "utf-8">
 	<title>Mutos </title>
 	<script type="text/javascript" src="/jquery/jquery.js"></script>
 	<link rel = "stylesheet" href = "/basicCss.css">
+	<style>
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        height: 100%;
+      }
+	  #g_map{
+		  height:300px;
+		  width:900px;
+	  }
+ </style>
 <head>
 <body>
 <?php
@@ -13,16 +29,22 @@
 <script>
 	$().ready(function(){
 		$('#top_header').load('/util/header.html');
-		$('#user_page_main_panel').load('user_page_profile.html');
+		$('#profile_panel').load('user_page_profile.html');
+		$('#g_map').load('/map/test.html');
+		
 	});
 </script>
 
 
 <div id = "top_header"></div>
+	
+
 <div id = 'user_page_main_panel'>
-	<div id = 'profile_panel'>
-	</div>
+	<div id = 'profile_panel'> </div>
+	<div id = 'g_map'>	</div>
 </div>
+</div>
+
 </body>
 
 
